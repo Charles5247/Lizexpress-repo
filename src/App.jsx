2ea2c4d1759f8d08515528a30fa4371bff46bfe9
Charles5247 <cjemeka2015@gmail.com>
2025-03-05 15:48:26 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="App-logo" alt="react logo" />
+        <img src={viteLogo} className="App-logo" alt="vite logo" />
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <button onClick={() => setCount((count) => count + 1)}>count is: {count}</button>
+        </p>
+        <p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          {' | '}
+          <a className="App-link" href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+            Learn Vite
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
